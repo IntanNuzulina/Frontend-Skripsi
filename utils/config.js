da9c82bceff654d65a82fdead5f26501be955672
Intan Nuzulina <intannuzulina@gmail.com>
2024-10-22 12:57:09 +0700

    edit file config.js

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,11 @@
 const BASE_URL =
   process.env.NEXT_PUBLIC_PRODUCTION === "production"
-    ? "http://202.10.40.136/api"
+    ? "http://api.intannuzulina.online/api"
     : "http://localhost:8000/api";
 
 const IMAGE_URL =
   process.env.NEXT_PUBLIC_PRODUCTION === "production"
-    ? "http://202.10.40.136/storage"
+    ? "http://api.intannuzulina.online/api"
     : "http://localhost:8000/storage";
 
 export { BASE_URL, IMAGE_URL };
