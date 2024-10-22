114aaee6319e3ca1c9d8e6154c3454d42d757fcf
Intan Nuzulina <intannuzulina@gmail.com>
2024-10-22 18:44:09 +0700

    fix error

--- app/order/page.jsx
+++ app/order/page.jsx
@@ -1,12 +1,60 @@
 "use client";
 
 import Navbar from "@/components/navbar";
+import useAuth from "@/hooks/useAuth";
 import { BASE_URL, IMAGE_URL } from "@/utils/config";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function OrderHistory() {
   const [order, setOrder] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const handlePayment = async (order) => {
+    setLoading(true);
+    try {
+      const response = await axios.post(BASE_URL + "/payment/charge", {
+        amount: order.harga,
+        email: user?.email,
+        phone: user?.phone,
+        user_id: user?.id,
+        buku_id: order?.buku.id,
+        qty: order?.qty,
+        alamat_penerima: user?.alamat,
+        token: order?.token,
+      });
+      const token = response.data.token;
+
+      setLoading(false);
+      window.snap.pay(token, {
+        onSuccess: function (result) {
+          /* You may add your own implementation here */
+          router.push("/order");
+          console.log(result);
+        },
+        onPending: function (result) {
+          /* You may add your own implementation here */
+          alert("wating your payment!");
+          console.log(result);
+        },
+        onError: function (result) {
+          /* You may add your own implementation here */
+          alert("payment failed!");
+          console.log(result);
+        },
+        onClose: function () {
+          /* You may add your own implementation here */
+          alert("you closed the popup without finishing the payment");
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,6 +71,20 @@ export default function OrderHistory() {
       }
     };
     fetchData();
+
+    const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
+
+    let scriptTag = document.createElement("script");
+    scriptTag.src = midtransScriptUrl;
+
+    const myMidtransClientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY;
+    scriptTag.setAttribute("data-client-key", myMidtransClientKey);
+
+    document.body.appendChild(scriptTag);
+
+    return () => {
+      document.body.removeChild(scriptTag);
+    };
   }, []);
   return (
     <>
@@ -66,7 +128,10 @@ export default function OrderHistory() {
                     <td>Rp. {item.harga}</td>
                     <td className="">
                       {item.status === "unpaid" ? (
-                        <button className="mx-auto w-16 h-7 block rounded-lg text-xs mb-1 text-white bg-green-600  hover:bg-green-700">
+                        <button
+                          className="mx-auto w-16 h-7 block rounded-lg text-xs mb-1 text-white bg-green-600  hover:bg-green-700"
+                          onClick={() => handlePayment(item)}
+                        >
                           Proses
                         </button>
                       ) : (
